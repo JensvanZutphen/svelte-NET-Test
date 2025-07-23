31f37a1cdc86fe7e29ef04177374a896d7509221
Jens van Zutphen <j.v.zutphen@gooskens.nl>
2025-07-23 09:49:30 +0200

    Initialize Svelte application with server and client setup. Added configuration files, project structure, and initial components for the Svelte frontend and ASP.NET backend. Included .env, .gitignore, and various configuration files for build and development environments.

---
+++ MySvelteApp.Client/src/routes/counter/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import PageTitle from "../components/PageTitle.svelte";
+
+    let currentCount = 0;
+
+    function incrementCount()
+    {
+        currentCount++;
+    }
+</script>
+
+<PageTitle>Counter</PageTitle>
+
+<h1>Counter</h1>
+
+<p role="status">Current count: {currentCount}</p>
+
+<button class="btn btn-primary" on:click={incrementCount}>Click me</button>
